6489635567f729a1f54d2af1bad74a0d139d3782
Mostofa321 <hmshumon123@gmail.com>
2022-11-08 21:01:35 +0600

    installed react-bootstrap and react-router-dom

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-info'>this is the heading one</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/contexts/UserContexts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserContexts = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UserContexts;
