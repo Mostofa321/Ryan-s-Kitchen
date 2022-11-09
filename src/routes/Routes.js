0453a03732ba6b9366a6a83dbf839353e5e29d05
Mostofa321 <hmshumon123@gmail.com>
2022-11-09 09:24:40 +0600

    added login form

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Blog from '../components/Blog/Blog';
 import Foods from '../components/Foods/Foods';
 import Home from '../components/Home/Home';
+import Login from '../components/Login/Login';
 import Main from '../layouts/Main';
 
 
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
             {
                 path: 'blog',
                 element: <Blog/>
+            },
+            {
+                path: '/login',
+                element: <Login/>
             }
         ]
     }
