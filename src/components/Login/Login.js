134b29f9bbbef4a597edaecd46202a83f357e0b3
Mostofa321 <hmshumon123@gmail.com>
2022-11-09 12:34:19 +0600

    added google login system and private route...

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContexts';
 
 const Login = () => {
+    const navigate = useNavigate();
+    let location = useLocation();
+    let { login, googleLogin } = useContext(AuthContext);
+    const [errMessage, setErrMessage] = useState(null)
+    const [successMessage, setSuccesMeassage] = useState(null)
+    // logit with email and password 
+    const loginUser = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value
+        const password = form.password.value;
+        if (!email || !password) {
+            return
+        };
+        login(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+                setSuccesMeassage('user logined successfully');
+                form.reset();
+                let from = location.state?.from?.pathname || "/";
+                navigate(from, { replace: true });
+            })
+            .catch(err => {
+                const errorMessage = err.message;
+                setErrMessage(errorMessage);
+            });
+
+    };
+    // login with google 
+    const loginGoogle = () => {
+        googleLogin()
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                let from = location.state?.from?.pathname || "/";
+                navigate(from, { replace: true });
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
     return (
         <div className='pt-md-5'>
             <div className='mx-auto p-5 form ' style={{ width: "500px", boxShadow: "rgba(240, 46, 170, 0.4) -5px 5px, rgba(240, 46, 170, 0.3) -10px 10px, rgba(240, 46, 170, 0.2) -15px 15px, rgba(240, 46, 170, 0.1) -20px 20px, rgba(240, 46, 170, 0.05) -25px 25px" }}>
-                <form >
+                <form onSubmit={loginUser}>
                     <h2 className='mb-4'>Login</h2>
                     <div className="mb-3">
                         <input type="email" name='email' className="form-control" placeholder='email' id="exampleInputEmail1" aria-describedby="emailHelp" />
@@ -13,9 +56,9 @@ const Login = () => {
                     <div className="mb-3">
                         <input type="password" name='password' className="form-control" placeholder='password' id="exampleInputPassword1" />
                     </div>
-                    {/* {
-                    successMessage ? <p className='text-success'>{successMessage}</p> : <p className='text-danger'>{errMessage}</p>
-                } */}
+                    {
+                        successMessage ? <p className='text-success'>{successMessage}</p> : <p className='text-danger'>{errMessage}</p>
+                    }
                     <button type="submit" className="btn btn-primary w-100">Sign In</button>
                 </form>
                 <div className='d-flex align-items-center'>
@@ -23,6 +66,7 @@ const Login = () => {
                     <h3 className=''>or</h3>
                     <hr className=' ms-2' style={{ border: "1px solid", width: "45%", height: "1px" }} />
                 </div>
+                <button onClick={loginGoogle} type="submit" className="btn btn-secondary w-100">Sign In With Google <i className="fa-brands fa-google ms-2 fs-4"></i></button>
                 <p className='mt-5'><small className='me-3'>dont have an account yet?</small><Link to='/signUp'>Sign Up</Link></p>
             </div>
         </div>
--- src/contexts/UserContexts.js
+++ src/contexts/UserContexts.js
@@ -1,21 +1,39 @@
-import { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import app from '../firebase/firebase.config';
 export const AuthContext = createContext('');
 
 
 
 export const auth = getAuth(app);
-const UserContexts = ({children}) => {
-    
-// sign up with email & password 
-const signUp = (email, password) =>{
-    
-    return  createUserWithEmailAndPassword(auth, email, password)
-};
-
-
-    const contextValue = {signUp}
+const UserContexts = ({ children }) => {
+    const [user, setUser] = useState('');
+    const [loading, setLoading] = useState(true);
+    // sign up with email & password 
+    const signUp = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    };
+    // sign in with email & password
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    };
+    // google login 
+    const googleProvider = new GoogleAuthProvider();
+    const googleLogin = ()=>{
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+    // get current user by using onAuthStateChanged 
+    useEffect(()=>{
+        const unSubscribe =  onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            setLoading(false);
+          });
+          return () => unSubscribe();
+      },[])
+    const contextValue = { user, loading, signUp, login, googleLogin }
     return (
         <AuthContext.Provider value={contextValue}>
             {
